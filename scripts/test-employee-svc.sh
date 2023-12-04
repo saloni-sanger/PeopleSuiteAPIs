654b261f3750ba95978edeb62f712ae4b3a58580
saloni-sanger <ssanger@seattleu.edu>
2023-12-03 19:22:43 -0800

    Added utility scripts

---
+++ scripts/test-employee-svc.sh
@@ -0,0 +1,2 @@
+echo "Response:"
+curl --header "Content-Type: application/json" --request POST --data '{"First Name":"Alice", "Last Name":"Doe", "Email Address":"alice.doe@peoplesuite.com", "Country": "US"}' http://localhost:8080/employees
