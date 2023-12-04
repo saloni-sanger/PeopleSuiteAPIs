d74beb7b229aa17acb9ffda62b289bfd3be03bc1
saloni-sanger <ssanger@seattleu.edu>
2023-12-03 19:22:15 -0800

    Worked on EmployeeService functionality

--- src/EmployeeService.py
+++ src/EmployeeService.py
@@ -1,30 +1,78 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from iso3166 import countries
+import os
 
 app = Flask(__name__)
-"""
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'mysql://mysql:3308' 
+
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'mysql://{}:{}@mysql:3306/peoplesuite'.format(os.environ.get('DB_USER'), os.environ.get('DB_PASS')) 
+#mysql protocol, mysql service on port 3306
 # mysql protocol to server at mysql (service name), will ask k8 DNS for IP of mysql (the service), then connect to IP it gets back on port 3308 (our service's IP port)
 
 db = SQLAlchemy(app) #db is a client instance configured from the mysql service
-class students(db.Model):
-   id = db.Column('student_id', db.Integer, primary_key = True)
-   name = db.Column(db.String(100))
-   city = db.Column(db.String(50))  
-   addr = db.Column(db.String(200))
-   pin = db.Column(db.String(10))
+#constructed using flask app so it can 'deeply integrate' with it
+class Employee(db.Model): #employee class will inherit db.Model's methods (db.Model is parent class, employee is child class)
+   __tablename__ = "Employee Table"
+   id = db.Column('employee_id', db.Integer, primary_key = True)
+   first = db.Column('first_name', db.String(50))
+   last = db.Column('last_name', db.String(50))
+   email = db.Column('email_address', db.String(100))  
+   country = db.Column('country', db.String(2))
+   # run validation on country code to make sure it's one of them (use library) when you get a POST, send error code if not
 
-def __init__(self, name, city, addr,pin): #creating an object for students table
-   self.name = name
-   self.city = city
-   self.addr = addr
-   self.pin = pin
+   def __init__(self, first, last, email, country): #constructor for SQLAlchemy or me, used for add
+      self.first = first
+      self.last = last
+      self.email = email
+      self.country = country
 
-db.create_all()
-"""
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+with app.app_context():
+   db.create_all()
+
+@app.route('/employees/health')
+def emp_health():
+   return "healthy"
+
+@app.route('/employees', methods = ['GET', 'POST']) #specifying methods means other requests get ignored
+def list():
+   if request.method == 'GET' :
+      return "NYI", 501
+      #return
+   if request.method == 'POST' :
+      #parse json to add employee to the table
+      #use the json sent in create object and put it in db
+      if not request.is_json :
+         return 'request body must contain JSON', 400 #request is not json, send bad request error
+
+      content = request.get_json() #parses incoming JSON request data and returns i8t as a python dictionary
+      if countries.get(content['Country']) == None :#check if country is in ISO-3166 library
+         return 'Country code must be in ISO-3166 format', 400 #request is bad
+      
+      first, last, email, country = content['First Name'], content['Last Name'], content['Email Address'], content['Country']
+      emp = Employee(first, last, email, country)
+      db.session.add(emp)#add does not take what __init__ takes, it takes an instance of a class extending db.Model
+      #it will 'ask' the object its type and fields and figure out what to do
+      db.session.commit()
+      created_object = Employee.query.filter_by(first = first, last = last, email = email, country = country).first() #no gaurantee it's the same obj, no duplication control/avoidance
+      #print(created_object) #test
+      if created_object is None :
+         return 'Failed to create object', 500 #internal server error
+      result = dict( #ordering is not insertion order (aplhabertical)
+         EmployeeID = created_object.id,
+         FirstName = created_object.first,
+         LastName = created_object.last,
+         EmailAddress = created_object.email,
+         Country = created_object.country,
+      )
+      #If you return a Python dictionary in a Flask view, the dictionary will automatically be converted to the JSON format for the response. (sentry.io)
+      return result, 201 #successful creation
+      #create json to send back (say the content type is json)
+      #return
+
+@app.route('/employees/<employee_id>', methods = ['GET'])
+def person(employee_id):
+   return "NYI", 501
+   #only one method, so do ur get here
 
 #i have code to build a docker image (Dockerfile)
 #to run in k8, 
