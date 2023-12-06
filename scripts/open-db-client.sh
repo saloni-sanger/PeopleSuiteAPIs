4c74a6edede822ba642a075a9ff6ca8e8024e260
saloni-sanger <ssanger@seattleu.edu>
2023-12-05 18:16:09 -0800

    Script for database client

--- scripts/open-db-client.sh
+++ scripts/open-db-client.sh
@@ -1,2 +1,3 @@
 echo "To create database, once in shell run \"CREATE DATABASE peoplesuite;\""
+#USE database_name;
 kubectl run -it --rm --image=mysql:5.6 --restart=Never mysql-client -- mysql -h mysql -ppassword
