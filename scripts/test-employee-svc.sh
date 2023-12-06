117335a690ca6265f6630900eea3f152075192ff
saloni-sanger <ssanger@seattleu.edu>
2023-12-05 23:04:25 -0800

    Fixed typo

--- scripts/test-employee-svc.sh
+++ scripts/test-employee-svc.sh
@@ -1,4 +1,4 @@
 echo "Response:"
-curl --header "Content-Type: application/json" --request POST --data '{"First Name":"Alice", "Last Name":"Doe", "Email Address":"alice.doe@peoplesuite.com", "Country": "US"}' http://localhost:8080/
+curl --header "Content-Type: application/json" --request POST --data '{"First Name":"Alice", "Last Name":"Doe", "Email Address":"alice.doe@peoplesuite.com", "Country": "US"}' http://localhost:8080/employees
 #curl http://localhost:8080/employees
 #for GET a list of all employees
